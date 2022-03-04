a8d023a59a2c184752cda5c022764010b6da6313
Sandra Murad <1171129@student.birzeit.edu>
2022-03-04 16:12:16 +0200

    initializing project folder

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const app = express()
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT
+app.listen(PORT,function (){
+    console.log('server is up and running on port:'+ PORT)
+})
